43733b62de0dbc20211e750e2c0fdc66e7919d80
level09 <level09@gmail.com>
2024-10-15 16:08:45 +0200

    Refactor version utils, when parsing pyproject toml file, only accept version if package name is bayanat

--- bayanat_cli/utils/version.py
+++ bayanat_cli/utils/version.py
@@ -6,7 +6,7 @@ def get_bayanat_version(app_dir: str) -> str:
     """Get Bayanat version using various methods."""
     app_path = Path(app_dir)
 
-    # Try to get version from installed package in Bayanat's environment
+    # Try to get version from the Bayanat application package
     try:
         version = subprocess.check_output(
             [str(app_path / 'env' / 'bin' / 'python'), '-c', 'import bayanat; print(bayanat.__version__)'],
@@ -16,7 +16,7 @@ def get_bayanat_version(app_dir: str) -> str:
     except subprocess.CalledProcessError:
         pass
 
-    # Try Git tags
+    # Try Git tags for the Bayanat application
     try:
         return subprocess.check_output(
             ['git', 'describe', '--tags', '--abbrev=0'],
@@ -26,12 +26,14 @@ def get_bayanat_version(app_dir: str) -> str:
     except subprocess.CalledProcessError:
         pass
 
-    # Try pyproject.toml
+    # Check pyproject.toml for the Bayanat application
     pyproject_path = app_path / 'pyproject.toml'
     if pyproject_path.is_file():
         try:
             with pyproject_path.open('rb') as f:
-                return tomli.load(f)['project']['version']
+                pyproject_data = tomli.load(f)
+                if pyproject_data.get('project', {}).get('name') == 'bayanat':
+                    return pyproject_data['project']['version']
         except (KeyError, tomli.TOMLDecodeError):
             pass
 
